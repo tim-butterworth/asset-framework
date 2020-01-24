7b0483beb4a0491aaa58d3bf66703ffc20bca63d
Tim <timothy.d.butterworth@gmail.com>
2020-01-23 20:32:18 -0700

    Initial Commit

---
+++ SampleHtmlFramework/SampleHtmlFramework.h
@@ -0,0 +1,19 @@
+//
+//  SampleHtmlFramework.h
+//  SampleHtmlFramework
+//
+//  Created by SWATHI MAKKENA on 1/23/20.
+//  Copyright © 2020 SWATHI MAKKENA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleHtmlFramework.
+FOUNDATION_EXPORT double SampleHtmlFrameworkVersionNumber;
+
+//! Project version string for SampleHtmlFramework.
+FOUNDATION_EXPORT const unsigned char SampleHtmlFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleHtmlFramework/PublicHeader.h>
+
+
